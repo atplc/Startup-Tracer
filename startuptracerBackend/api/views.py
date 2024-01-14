7b71d8b30c45b1518c2fb2b2810b7a10c8a65530
Atul18341 <atulkumar987613@gmail.com>
2024-01-14 22:08:46 +0530

    View Updated

--- startuptracerBackend/api/views.py
+++ startuptracerBackend/api/views.py
@@ -3,4 +3,4 @@ from django.http import JsonResponse
 # Create your views here.
 
 def Home(request):
-        return JsonResponse({'response':'Hello world.Welcome to Django Project.'})
+        return JsonResponse({'response':'Hello world.Welcome to my Django Project.'})
